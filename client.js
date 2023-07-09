e31821d262b6ca42afa3c2834b201c11e2c96a4d
justqstn <128418414+justqstn@users.noreply.github.com>
2023-07-09 20:48:40 +0300

    Update client.js

--- client.js
+++ client.js
@@ -2,9 +2,9 @@
 //let System = importNamespace('System');
 
 // êîíñòàíòû
-let WaitingPlayersTime = 10;
-let BuildBaseTime = 30;
-let GameModeTime = 600;
+let WaitingPlayersTime = 2;
+let BuildBaseTime = 2;
+let GameModeTime = 2;
 let EndOfMatchTime = 20;
 
 // êîíñòàíòû èìåí
@@ -75,11 +75,11 @@ LeaderBoard.TeamLeaderBoardValue = {
 	ShortDisplayName: "Statistics\Deaths"
 };
 // âåñ êîìàíäû â ëèäåðáîðäå
-LeaderBoard.TeamWeightGetter.Set(function(team) {
+LeaderBoard.TeamWeightGetter.Set(function (team) {
 	return team.Properties.Get("Deaths").Value;
 });
 // âåñ èãðîêà â ëèäåðáîðäå
-LeaderBoard.PlayersWeightGetter.Set(function(player) {
+LeaderBoard.PlayersWeightGetter.Set(function (player) {
 	return player.Properties.Get("Kills").Value;
 });
 
@@ -89,86 +89,86 @@ Ui.GetContext().TeamProp2.Value = { Team: "Red", Prop: "Deaths" };
 
 // ðàçðåøàåì âõîä â êîìàíäû ïî çàïðîñó
 const props = ["Kills", "Deaths", "Scores", "KD"];
-Teams.OnRequestJoinTeam.Add(function(player,team){
+Teams.OnRequestJoinTeam.Add(function (player, team) {
 	for (indx in props) {
-        player.Properties.Get(props[indx]).Value = Properties.GetContext().Get(props[indx] + player.Id).Value || 0;
+		player.Properties.Get(props[indx]).Value = Properties.GetContext().Get(props[indx] + player.Id).Value || 0;
 		Properties.GetContext().Get(props[indx] + player.Id).Value = null;
 		saved_id.Value.replace(player.Id + "/", "");
-    }
+	}
 	team.Add(player);
 });
 // ñïàâí ïî âõîäó â êîìàíäó
-Teams.OnPlayerChangeTeam.Add(function(player){ player.Spawns.Spawn()});
+Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn() });
 
-Players.OnPlayerDisconnected.Add(function(player) {
-    for (indx in props) {
-        Properties.GetContext().Get(props[indx] + player.Id).Value = player.Properties.Get(props[indx]).Value;
-	    saved_id.Value += player.Id + "/";
-    }
+Players.OnPlayerDisconnected.Add(function (player) {
+	for (indx in props) {
+		Properties.GetContext().Get(props[indx] + player.Id).Value = player.Properties.Get(props[indx]).Value;
+		saved_id.Value += player.Id + "/";
+	}
 });
 
 // äåëàåì èãðîêîâ íåóÿçâèìûìè ïîñëå ñïàâíà
-let immortalityTimerName="immortality";
-Spawns.GetContext().OnSpawn.Add(function(player){
-	player.Properties.Immortality.Value=true;
-	timer=player.Timers.Get(immortalityTimerName).Restart(5);
+let immortalityTimerName = "immortality";
+Spawns.GetContext().OnSpawn.Add(function (player) {
+	player.Properties.Immortality.Value = true;
+	timer = player.Timers.Get(immortalityTimerName).Restart(5);
 });
-Timers.OnPlayerTimer.Add(function(timer){
-    if (timer.Id == "combo") {
-        timer.Player.Properties.Get("combo").Value = 0;
-    }
+Timers.OnPlayerTimer.Add(function (timer) {
+	if (timer.Id == "combo") {
+		timer.Player.Properties.Get("combo").Value = 0;
+	}
 	else if (timer.Id == immortalityTimerName) {
-        timer.Player.Properties.Immortality.Value=false;
-    }
+		timer.Player.Properties.Immortality.Value = false;
+	}
 });
 
 // ïîñëå êàæäîé ñìåðòè èãðîêà îòíèìàåì îäíó ñìåðòü â êîìàíäå
-Properties.OnPlayerProperty.Add(function(context, value) {
+Properties.OnPlayerProperty.Add(function (context, value) {
 	if (value.Name !== "Deaths") return;
 	if (context.Player.Team == null) return;
 	context.Player.Team.Properties.Get("Deaths").Value--;
 });
 // åñëè â êîìàíäå êîëè÷åñòâî ñìåðòåé çàíóëèëîñü òî çàâåðøàåì èãðó
-Properties.OnTeamProperty.Add(function(context, value) {
+Properties.OnTeamProperty.Add(function (context, value) {
 	if (value.Name !== "Deaths") return;
 	if (value.Value <= 0) SetEndOfMatchMode();
 });
 
 // ñ÷åò÷èê ñïàâíîâ
-Spawns.OnSpawn.Add(function(player) {
+Spawns.OnSpawn.Add(function (player) {
 	++player.Properties.Spawns.Value;
 });
 // ñ÷åò÷èê ñìåðòåé
-Damage.OnDeath.Add(function(player) {
+Damage.OnDeath.Add(function (player) {
 	++player.Properties.Deaths.Value;
-  	player.Properties.Get("KD").Value = player.Properties.Deaths.Value > 0 ? Math.round(player.Properties.Kills.Value / player.Properties.Deaths.Value * 100) / 100 : player.Properties.Kills.Value;
+	player.Properties.Get("KD").Value = player.Properties.Deaths.Value > 0 ? Math.round(player.Properties.Kills.Value / player.Properties.Deaths.Value * 100) / 100 : player.Properties.Kills.Value;
 });
 // ñ÷åò÷èê óáèéñòâ
-Damage.OnKill.Add(function(player, killed) {
+Damage.OnKill.Add(function (player, killed) {
 	if (killed.Team != player.Team) {
-        	player.Properties.Get("combo").Value += 1;
+		player.Properties.Get("combo").Value += 1;
 		++player.Properties.Kills.Value;
 		player.Properties.Get("KD").Value = player.Properties.Deaths.Value > 0 ? Math.round(player.Properties.Kills.Value / player.Properties.Deaths.Value * 100) / 100 : player.Properties.Kills.Value;
-        	player.Properties.Scores.Value += 100 * player.Properties.Get("combo").Value;
-        	player.Timers.Get("combo").Restart(4);
+		player.Properties.Scores.Value += 100 * player.Properties.Get("combo").Value;
+		player.Timers.Get("combo").Restart(4);
 	}
 });
 
 // íàñòðîéêà ïåðåêëþ÷åíèÿ ðåæèìîâ
-mainTimer.OnTimer.Add(function() {
+mainTimer.OnTimer.Add(function () {
 	switch (stateProp.Value) {
-	case WaitingStateValue:
-		SetBuildMode();
-		break;
-	case BuildModeStateValue:
-		SetGameMode();
-		break;
-	case GameStateValue:
-		SetEndOfMatchMode();
-		break;
-	case EndOfMatchStateValue:
-		RestartGame();
-		break;
+		case WaitingStateValue:
+			SetBuildMode();
+			break;
+		case BuildModeStateValue:
+			SetGameMode();
+			break;
+		case GameStateValue:
+			SetEndOfMatchMode();
+			break;
+		case EndOfMatchStateValue:
+			RestartGame();
+			break;
 	}
 });
 
@@ -177,16 +177,16 @@ SetWaitingMode();
 
 function CalculateBest(name) {
 	let enm = Players.GetEnumerator();
-    this.id = "";
+	this.id = "";
 	this.nickname = "";
 	this.score = -1;
-    while (enm.moveNext()) {
-        if (enm.Current.Properties.Get(name).Value > this.score) {
-            this.score = enm.Current.Properties.Get(name).Value
-            this.id = enm.Current.Id;
+	while (enm.moveNext()) {
+		if (enm.Current.Properties.Get(name).Value > this.score) {
+			this.score = enm.Current.Properties.Get(name).Value
+			this.id = enm.Current.Id;
 			this.nickname = enm.Current.NickName;
-        }
-    }
+		}
+	}
 }
 
 // ñîñòîÿíèÿ èãðû
@@ -197,8 +197,7 @@ function SetWaitingMode() {
 	mainTimer.Restart(WaitingPlayersTime);
 }
 
-function SetBuildMode() 
-{
+function SetBuildMode() {
 	stateProp.Value = BuildModeStateValue;
 	Ui.GetContext().Hint.Value = "Hint/BuildBase";
 	let inventory = Inventory.GetContext();
@@ -212,8 +211,7 @@ function SetBuildMode()
 	Spawns.GetContext().enable = true;
 	SpawnTeams();
 }
-function SetGameMode() 
-{
+function SetGameMode() {
 	stateProp.Value = GameStateValue;
 	Ui.GetContext().Hint.Value = "Hint/AttackEnemies";
 
@@ -237,29 +235,27 @@ function SetGameMode()
 	SpawnTeams();
 }
 function SetEndOfMatchMode() {
-    try {
-        let saved_id_arr = saved_id.Value.split("/");
-        for (indx in saved_id_arr) {
-            for (i in props) {
-                Properties.GetContext().Get(props[i] + saved_id_arr[indx]).Value = null;
-            }
-        }
+	let saved_id_arr = saved_id.Value.split("/");
+	for (indx in saved_id_arr) {
+		for (i in props) {
+			Properties.GetContext().Get(props[i] + saved_id_arr[indx]).Value = null;
+		}
+	}
 
-		let top1_kills = new CalculateBest("Kills");
-		let top1_kd = new CalculateBest("KD");
-		let top1_scores = new CalculateBest("Scores")
+	let top1_kills = new CalculateBest("Kills");
+	let top1_kd = new CalculateBest("KD");
+	let top1_scores = new CalculateBest("Scores")
 
-        msg.Show("<B>Топ-1 по убийствам:</B> " + top1_kills.nickname + "\n<i>Счет: " + top1_kills.score + "</i>\n\n\n<B>Топ-1 по K/D:</B> " + top1_kd.nickname + "\n<i>Счет: " + top1_kd.score + "</i>\n\n\n<B>Топ-1 по очкам:</B> " + top1_scores.nickname + "\n<i>Счет: " + top1_scores.score + "</i>", "<B>Игра окончена!</B>");
+	msg.Show("<B>Топ-1 по убийствам:</B> " + top1_kills.nickname + "\n<i>Счет: " + top1_kills.score + "</i>\n\n\n<B>Топ-1 по K/D:</B> " + top1_kd.nickname + "\n<i>Счет: " + top1_kd.score + "</i>\n\n\n<B>Топ-1 по очкам:</B> " + top1_scores.nickname + "\n<i>Счет: " + top1_scores.score + "</i>", "<B>Игра окончена!</B>");
 
-        stateProp.Value = EndOfMatchStateValue;
-        Ui.GetContext().Hint.Value = "Hint/EndOfMatch";
+	stateProp.Value = EndOfMatchStateValue;
+	Ui.GetContext().Hint.Value = "Hint/EndOfMatch";
 
-        let spawns = Spawns.GetContext();
-        spawns.enable = false;
-        spawns.Despawn();
-        Game.GameOver(LeaderBoard.GetTeams());
-        mainTimer.Restart(EndOfMatchTime);
-    } catch(e) { msg.Show(e.name + " " + e.message); }
+	let spawns = Spawns.GetContext();
+	spawns.enable = false;
+	spawns.Despawn();
+	Game.GameOver(LeaderBoard.GetTeams());
+	mainTimer.Restart(EndOfMatchTime);
 }
 function RestartGame() {
 	Game.RestartGame();
